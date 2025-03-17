ef0e30f2f6a03f75580d2acdb46857bbe8415bcc
NathanaelOsborne <yudi0sath@gmail.com>
2025-03-17 14:12:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.title('Dermatology Machine Learning')
+def main():
+  st.title('Dermatology Machine Learning')
+  st.write('This app using machine learning')
 
-st.write('Hello world!')
+if __name__(self, 
